a3325ec1721c8b3e7f5023a94aeb6466646f0004
ArwaI1 <s-arwa.mater@zewailcity.edu.eg>
2026-03-22 23:50:20 +0200

    Initial commit with DagsHub setup

--- check_threshold.py
+++ check_threshold.py
@@ -1,17 +1,15 @@
 import sys
 import mlflow
 
-# Read the Run ID from the artifact downloaded by GitHub Actions
 with open("model_info.txt", "r") as f:
     run_id = f.read().strip()
 
-# Fetch the run data from MLflow
+# Fetch from DagsHub
 run = mlflow.get_run(run_id)
 accuracy = run.data.metrics.get("accuracy", 0.0)
 
 print(f"Run ID: {run_id}, Accuracy: {accuracy}")
 
-# Logic to fail the pipeline if accuracy is too low
 if accuracy < 0.85:
     print("Error: Accuracy below 0.85 threshold. Failing the pipeline.")
     sys.exit(1)
